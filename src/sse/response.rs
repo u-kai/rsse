97feeff04471f21f50f88d110f91f7097c46c0d9
u-kai <76635578+u-kai@users.noreply.github.com>
2023-08-06 10:42:44 +0900

    add event

--- src/sse/response.rs
+++ src/sse/response.rs
@@ -6,6 +6,12 @@ pub enum SseResponse {
 
 impl SseResponse {
     pub fn from_line(line: &str) -> Self {
+        if line.starts_with("data:") {
+            return Self::Data(line.replace("data:", "").trim().to_string());
+        }
+        if line.starts_with("event:") {
+            return Self::Event(line.replace("event:", "").trim().to_string());
+        }
         Self::Event(line[6..].to_string().trim_end().to_string())
     }
 }
@@ -17,6 +23,14 @@ mod tests {
     #[test]
     fn sse_のdataの場合() {
         let sse_data = "data: hello world\n\n";
+
+        let sut = SseResponse::from_line(sse_data);
+
+        assert_eq!(SseResponse::Data("hello world".to_string()), sut);
+    }
+    #[test]
+    fn sse_のeventの場合() {
+        let sse_data = "event: hello world\n\n";
 
         let sut = SseResponse::from_line(sse_data);
 
